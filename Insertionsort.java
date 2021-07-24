1b0ae06b907a8f940b50de58c0e6cc03555ce3f0
Amir Habib <65022657+AmirH-27@users.noreply.github.com>
2021-07-24 18:54:46 +0600

    Update Insertionsort.java

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 
-public class Insertionsort {
-    static void InsertionSort(int[] array,int size) {
+public class Practice {
+    static void InsertionSortDesc(int[] array,int size) {
+        for (int i = 0; i < size; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j>=0 && array[j] < temp) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+            System.out.print("Step-"+i+": ");
+            for(int k=0;k<size;k++){
+                System.out.print(array[k]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------------");
+    }
+    static void InsertionSortAsc(int[] array,int size) {
         for (int i = 0; i < size; i++) {
             int temp = array[i];
             int j = i - 1;
@@ -29,14 +46,15 @@ public class Insertionsort {
             array[i] = input.nextInt();
         }
 
-        System.out.println("Array : ");
+        System.out.print("Array : ");
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
 
         System.out.println();
-        System.out.println("Insertion sort Ascending : ");
-        InsertionSort(array, size);
+        System.out.println("Insertion sort Descending : ");
+        InsertionSortDesc(array, size);
+        InsertionSortAsc(array, size);
     }
 
 }
